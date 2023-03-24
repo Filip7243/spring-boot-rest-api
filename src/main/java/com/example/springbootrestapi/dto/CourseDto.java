82b2d1b39c3f1b0bb01f0109a91730884aa094d6
Filip7243 <filip7243@gmail.com>
2023-03-24 17:51:21 +0100

    created dtos

---
+++ src/main/java/com/example/springbootrestapi/dto/CourseDto.java
@@ -0,0 +1,23 @@
+package com.example.springbootrestapi.dto;
+
+public class CourseDto {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
