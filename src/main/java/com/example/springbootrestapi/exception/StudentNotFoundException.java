bdb7da47d9a7c8b003a184a57e0390b320b80be1
Filip <filip7243@gmail.com>
2023-03-24 21:54:04 +0100

    modified excpetion msg

--- src/main/java/com/example/springbootrestapi/exception/StudentNotFoundException.java
+++ src/main/java/com/example/springbootrestapi/exception/StudentNotFoundException.java
@@ -1,8 +1,8 @@
 package com.example.springbootrestapi.exception;
 
-public class StudentNotFoundException extends RuntimeException{
+public class StudentNotFoundException extends RuntimeException {
 
     public StudentNotFoundException(Long id) {
-
+        super("Student with id: " + id + "not found!");
     }
 }
