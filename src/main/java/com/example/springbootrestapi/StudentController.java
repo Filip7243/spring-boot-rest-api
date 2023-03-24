37a4ee0086edacb05f2883b2f8ce27631efb2d66
Filip <filip7243@gmail.com>
2023-03-24 22:13:22 +0100

    created rest controller for studnet

---
+++ src/main/java/com/example/springbootrestapi/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.springbootrestapi;
+
+import com.example.springbootrestapi.dto.StudentDto;
+import com.example.springbootrestapi.service.CourseService;
+import com.example.springbootrestapi.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    private StudentService studentService;
+    private CourseService courseService;
+
+    public StudentController(StudentService studentService, CourseService courseService) {
+        this.studentService = studentService;
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentDto>> findAll() {
+        return ResponseEntity.ok(studentService.findAllStudents());
+    }
+}
