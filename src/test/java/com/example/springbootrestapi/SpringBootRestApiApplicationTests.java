6c50380ed1f0a53976d7ab912680c476ebf614c4
Filip <filip7243@gmail.com>
2023-03-24 23:04:08 +0100

    added test for checking loaded context

--- src/test/java/com/example/springbootrestapi/SpringBootRestApiApplicationTests.java
+++ src/test/java/com/example/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.springbootrestapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
 class SpringBootRestApiApplicationTests {
+
+    @Autowired
+    private StudentController studentController;
 
     @Test
     void contextLoads() {
+        assertThat(studentController).isNotNull();
     }
 
 }
