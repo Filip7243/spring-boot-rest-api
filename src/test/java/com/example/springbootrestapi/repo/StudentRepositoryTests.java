412b3b95b56804e9119e7d258ab5628c0cc869ab
Filip <filip7243@gmail.com>
2023-03-24 20:02:38 +0100

    added tests and modified db connection config

--- src/test/java/com/example/springbootrestapi/repo/StudentRepositoryTests.java
+++ src/test/java/com/example/springbootrestapi/repo/StudentRepositoryTests.java
@@ -22,19 +22,24 @@ public class StudentRepositoryTests {
     @Autowired
     private CourseRepository courseRepository;
 
+    private Student s1;
+    private Student s2;
+    private Student s3;
     private Course c1;
     private Course c2;
     private Course c3;
+    private Course c4;
 
     @BeforeEach
     void setUp() {
-        Student s1 = new Student("00000", "John", "Doe", "john@mail.com");
-        Student s2 = new Student("00001", "Mark", "Doe", "Mark@mail.com");
-        Student s3 = new Student("00002", "Steve", "Doe", "Steve@mail.com");
+        s1 = new Student("00000", "John", "Doe", "john@mail.com");
+        s2 = new Student("00001", "Mark", "Doe", "Mark@mail.com");
+        s3 = new Student("00002", "Steve", "Doe", "Steve@mail.com");
 
         c1 = new Course("Course 1", "C1 description");
         c2 = new Course("Course 2", "C2 description");
         c3 = new Course("Course 3", "C3 description");
+        c4 = new Course("Course 4", "C4 description");
 
         studentRepository.save(s1);
         studentRepository.save(s2);
@@ -43,6 +48,7 @@ public class StudentRepositoryTests {
         courseRepository.save(c1);
         courseRepository.save(c2);
         courseRepository.save(c3);
+        courseRepository.save(c4);
 
         s1.addCourseToStudent(c1);
         s1.addCourseToStudent(c2);
@@ -53,10 +59,37 @@ public class StudentRepositoryTests {
     }
 
     @Test
-    public void itShouldFindStudentsBySpecificCourse() {
+    public void itShouldFindTwoStudentsBySpecificCourse() {
         List<Student> foundStudents = studentRepository.findStudentsByCoursesContaining(c1);
 
         assertThat(foundStudents).isNotNull();
         assertThat(foundStudents.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void itShouldNotFindAnyStudent() {
+        List<Student> foundStudents = studentRepository.findStudentsByCoursesContaining(c4);
+
+        assertThat(foundStudents).isNotNull();
+        assertThat(foundStudents.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void itShouldFindStudentWithoutCourse() {
+        List<Student> foundStudents = studentRepository.findStudentsWithNoCoursesEnrolled();
+
+        assertThat(foundStudents).isNotNull();
+        assertThat(foundStudents.size()).isEqualTo(1);
+        assertThat(foundStudents.get(0).getAlbumId()).isEqualTo("00002");
+    }
+
+    @Test
+    public void itShouldNotFindStudentWithoutCourse() {
+        s3.addCourseToStudent(c1);
+
+        List<Student> foundStudents = studentRepository.findStudentsWithNoCoursesEnrolled();
+
+        assertThat(foundStudents).isNotNull();
+        assertThat(foundStudents.size()).isEqualTo(0);
     }
 }
