37fe63f697e1d0ebb5fb45e85cffe15881221f89
Filip7243 <filip7243@gmail.com>
2023-03-24 17:40:09 +0100

    model already done

---
+++ src/main/java/com/example/springbootrestapi/model/Course.java
@@ -0,0 +1,63 @@
+package com.example.springbootrestapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToMany(fetch = LAZY, mappedBy = "courses")
+    private Set<Student> students = new HashSet<>();
+
+    public Course() {
+    }
+
+    public Course(String name, String description, Set<Student> students) {
+        this.id = null;
+        this.name = name;
+        this.description = description;
+        this.students = students;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
---
+++ src/main/java/com/example/springbootrestapi/model/Student.java
@@ -0,0 +1,100 @@
+package com.example.springbootrestapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String albumId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @ManyToMany(fetch = LAZY)
+    @JoinTable(
+            name = "students_courses",
+            joinColumns = {@JoinColumn(name = "student_id")},
+            inverseJoinColumns = {@JoinColumn(name = "course_id")}
+    )
+    private Set<Course> courses = new HashSet<>();
+
+    public Student() {
+    }
+
+    public Student(String albumId, String firstName, String lastName, String email) {
+        this.id = null;
+        this.albumId = albumId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void addCourseToStudent(Course course) {
+        if (this.courses.size() <= 5 && course.getStudents().size() <= 50) {
+            course.getStudents().add(this);
+            this.courses.add(course);
+        }
+        throw new RuntimeException("Maximum number of courses reached!");
+    }
+
+    public void removeCourseFromStudent(Course course) {
+        course.getStudents().remove(this);
+        this.courses.remove(course);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+}
