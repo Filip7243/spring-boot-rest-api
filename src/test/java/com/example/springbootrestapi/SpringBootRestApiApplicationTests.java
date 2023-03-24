90ad635b17d9f3251026a2dfe097d358d017e8d0
Filip <filip7243@gmail.com>
2023-03-24 23:51:52 +0100

    imporvoed tests

--- src/test/java/com/example/springbootrestapi/SpringBootRestApiApplicationTests.java
+++ src/test/java/com/example/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.springbootrestapi;
 
+import com.example.springbootrestapi.controller.StudentController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/com/example/springbootrestapi/controller/StudentControllerTests.java
+++ src/test/java/com/example/springbootrestapi/controller/StudentControllerTests.java
@@ -2,13 +2,18 @@ package com.example.springbootrestapi.controller;
 
 import com.example.springbootrestapi.dto.StudentDto;
 import com.example.springbootrestapi.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
@@ -16,6 +21,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -56,5 +62,23 @@ public class StudentControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void shouldReturnJsonWithCreatedUserAndStatusCreated() throws Exception {
+        StudentDto student = new StudentDto();
+        student.setAlbumId("00000");
+        student.setFirstName("test");
+        student.setLastName("test");
+        student.setEmail("test");
+
+        when(studentService.createStudent(student)).thenReturn(student);
+
+        mvc.perform(post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(student)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
     }
 }
